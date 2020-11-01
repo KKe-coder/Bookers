af91ea2b8a38ffdedf466286acd4764bf1005e7f
carumag726@gmail.com <ec2-user@ip-172-31-73-97.ec2.internal>
2020-11-01 11:52:03 +0000

    [Add] Createbook]-> show.html.erb and index 繰り返し出力

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_083804) do
+ActiveRecord::Schema.define(version: 2020_11_01_113900) do
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
