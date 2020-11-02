d6db74fdd13b8c879fd62c586367b85e7e02f39a
carumag726@gmail.com <ec2-user@ip-172-31-73-97.ec2.internal>
2020-11-02 12:58:39 +0000

    [Update]edit.html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    binding.pry
+    book.update(book_params)
+    redirect_to book_path(book), notice: `hogehoge`
   end
 
   private
