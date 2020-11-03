127f59151a9853bff122ac0064d8a09217072868
carumag726@gmail.com <ec2-user@ip-172-31-73-97.ec2.internal>
2020-11-03 07:08:00 +0000

    [Add]destroy 'Are you sure?'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,15 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    binding.pry
     book.update(book_params)
-    redirect_to book_path(book), notice: `hogehoge`
+    binding.pry
+    redirect_to book_path(book.id), notice: `hogehoge`
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: `Book was succesfully destroyed.`
   end
 
   private
