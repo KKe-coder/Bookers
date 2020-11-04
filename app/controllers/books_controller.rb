bfe43da3dcb62f377d674f06323805dc632c2551
carumag726@gmail.com <ec2-user@ip-172-31-73-97.ec2.internal>
2020-11-04 04:40:53 +0000

    [Update]エラーメッセージ表示以外の部分✓完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,8 +17,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice: "Book was successfully created."
+    if book.save
+      redirect_to book_path(book.id), notice: "Book was successfully created."
+    else
+      render :index
+    end
   end
 
   def edit
